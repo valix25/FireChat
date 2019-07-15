9c17f2dd306045c297505c46384eab9cd6acb91b
Valentin Vasiliu <vali15tnt51v@gmail.com>
2019-07-15 11:36:08 +0200

    Set up Sign in by email feature. Need to fix Splash style theme.

---
+++ app/src/main/java/com/walle/firechat/SplashActivity.kt
@@ -0,0 +1,19 @@
+package com.walle.firechat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import org.jetbrains.anko.startActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if(FirebaseAuth.getInstance().currentUser == null) {
+            startActivity<SignInActivity>()
+        } else {
+            startActivity<MainActivity>()
+        }
+        finish()
+    }
+}
