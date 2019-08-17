13e5fe696d8b78ff5cd097d9a69b4bfa43cdedd5
Valentin Vasiliu <vali15tnt51v@gmail.com>
2019-08-17 19:00:06 +0200

    Added search bar UI and tested response

--- app/src/main/java/com/walle/firechat/fragment/PeopleFragment.kt
+++ app/src/main/java/com/walle/firechat/fragment/PeopleFragment.kt
@@ -2,6 +2,9 @@ package com.walle.firechat.fragment
 
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,11 +24,16 @@ import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.fragment_people.*
 import org.jetbrains.anko.support.v4.startActivity
+import com.mancj.materialsearchbar.MaterialSearchBar
+import android.view.Gravity
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import kotlinx.android.synthetic.main.fragment_people.view.*
+import org.jetbrains.anko.support.v4.longToast
 
 
 class PeopleFragment : Fragment() {
-    // TODO: add search bar to search people
-
     // Remember the firestore listener in order to remove it later
     private lateinit var userListenerRegistration: ListenerRegistration
 
@@ -33,12 +41,63 @@ class PeopleFragment : Fragment() {
 
     private lateinit var peopleSection: Section
 
+    private val lastSearches: List<String>? = null
+    private val drawer: DrawerLayout? = null
+
+    private lateinit var searchBarObj: android.widget.SearchView
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
         userListenerRegistration = FirestoreUtil.addUsersListener(this.activity!!, this::updateRecyclerView)
 
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_people, container, false)
+        val view = inflater.inflate(R.layout.fragment_people, container, false)
+        view.searchBar.setOnSearchActionListener(object: MaterialSearchBar.OnSearchActionListener {
+            override fun onButtonClicked(buttonCode: Int) {
+                when (buttonCode) {
+                    MaterialSearchBar.BUTTON_NAVIGATION -> {
+                    }
+                    MaterialSearchBar.BUTTON_SPEECH -> {
+                    }
+                    MaterialSearchBar.BUTTON_BACK -> searchBar.disableSearch()
+                }
+            }
+
+            override fun onSearchStateChanged(enabled: Boolean) {
+                Log.i("search_query", "searchStateChanged: $enabled")
+            }
+
+            override fun onSearchConfirmed(text: CharSequence?) {
+                Log.i("search_query", "searchConfirmed: $text")
+            }
+
+        })
+        view.searchBar.addTextChangeListener(object: TextWatcher {
+            override fun afterTextChanged(p0: Editable?) {
+                Log.i("search_query", "afterTextChanged: $p0")
+            }
+
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                Log.i("search_query", "onTextChanged: $p0")
+            }
+
+        })
+//        view.searchBar.setOnQueryTextListener(object : android.widget.SearchView.OnQueryTextListener {
+//            override fun onQueryTextSubmit(p0: String?): Boolean {
+//                print("Text submitted: $p0")
+//                Log.i("query", " text submitted: $p0")
+//                return false
+//            }
+//
+//            override fun onQueryTextChange(p0: String?): Boolean {
+//                print("Text changed: $p0")
+//                return false
+//            }
+//        })
+        return view
     }
 
     override fun onDestroy() {
@@ -69,7 +128,7 @@ class PeopleFragment : Fragment() {
         }
     }
 
-    private val onItemClick = OnItemClickListener{ item, view ->
+    private val onItemClick = OnItemClickListener{ item, _ ->
         if(item is PersonItem) {
             startActivity<ChatActivity>(
                 AppConstants.USER_NAME to item.user.name,
